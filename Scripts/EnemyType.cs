145208058535b1cc373c4c88a1195e861a9792e9
CIown69 <connorjw28@gmail.com>
2025-10-18 14:58:58 -0600

    Add files via upload

---
+++ Scripts/EnemyType.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.Events;
+using static UnityEngine.RuleTile.TilingRuleOutput;
+
+namespace Shmup
+{
+      [CreateAssetMenu(fileName = "EnemyType", menuName = "Shmup/EnemyType", order = 0)]
+       public class EnemyType : ScriptableObject
+       {
+            public GameObject enemyPrefab;
+            public GameObject weaponPrefab;
+            public float speed = 2f;
+       }
+}
